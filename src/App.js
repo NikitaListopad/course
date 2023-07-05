37a0259478f3cb1598649a8dac948c0d706e98b2
Mykyta Lystopad <listopad.nikita96@gmail.com>
2023-07-05 18:05:28 +0300

    add materials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {Card} from "./components/Card";
 import './shared/styles/styles.scss';
+import {StateComponent} from "./components/StateComponent";
 function App() {
 
   return (
       <div className="App">
-        <Card isCardHovered={true}/>
+        {/*<Card isCardHovered={true}/>*/}
+        <StateComponent
+            title='React JS'
+        />
       </div>
   );
 }
